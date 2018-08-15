9b36bbd7e9a0bf6221c989d088961d960a0f556e
Christopher Arsenault <christopher.arsenault82@gmail.com>
2018-08-15 09:48:13 -0400

    added full house regex and marking scores to not be scored again

--- script.js
+++ script.js
@@ -14,26 +14,65 @@ $(document).ready(function() {
     }
 
     let scoreCard = {
-        one: 0,
-        two: 0,
-        three: 0,
-        four: 0,
-        five: 0,
-        six: 0,
+        one: {
+            value: 0,
+            scored: false
+        },
+        two: {
+            value: 0,
+            scored: false
+        },
+        three: {
+            value: 0,
+            scored: false
+        },
+        four: {
+            value: 0,
+            scored: false
+        },
+        five: {
+            value: 0,
+            scored: false
+        },
+        six: {
+            value: 0,
+            scored: false
+        },
         topScoreTotal: 0,
         bonus: 0,
-        threeKind: 0,
-        fourKind: 0,
-        fullHouse: 0,
-        small: 0,
-        large: 0,
-        yahtzee: 0,
-        chance: 0,
+        threeKind: {
+            value: 0,
+            scored: false
+        },
+        fourKind: {
+            value: 0,
+            scored: false
+        },
+        fullHouse: {
+            value: 0,
+            scored: false
+        },
+        small: {
+            value: 0,
+            scored: false
+        },
+        large: {
+            value: 0,
+            scored: false
+        },
+        yahtzee: {
+            value: 0,
+            scored: false
+        },
+        chance: {
+            value: 0,
+            scored: false
+        },
         bottomScoreTotal: 0,
         totalScore: 0,
         sumDice: function (choice) {
             for (let i = 0; i < 5; i++) {
-                this[choice] += diceRoll.dice[i];
+                this[choice].value += diceRoll.dice[i];
                 this.bottomScoreTotal += diceRoll.dice[i];
             }
         },
@@ -43,84 +82,110 @@ $(document).ready(function() {
             $(`#${section}-total-score`).append(`<h3>${this[scoreTotal]}</h3>`);
             $(`#total-score-score`).empty();
             $(`#total-score-score`).append(`<h3>${this.totalScore}</h3>`);
-            $(`#${selection}`).append(`<h3>${this[selection]}</h3>`);
+            $(`#${selection}`).append(`<h3>${this[selection].value}</h3>`);
         },
         addTopScore: function (selection) {
-            let value = 0;
-            switch(selection) {
-                case "one":
-                    value = 1;
-                    break;
-                case "two":
-                    value = 2;
-                    break;
-                case "three":
-                    value = 3;
-                    break;
-                case "four":
-                    value = 4;
-                    break;
-                case "five":
-                    value = 5;
-                    break;
-                case "six":
-                    value = 6;
-                    break;
-            }
-            for (let i = 0; i < 5; i++) {
-                if (diceRoll.dice[i] === value) {
-                    this[selection] += value;
-                    this.topScoreTotal += value;
+            if (this[selection].scored === false) {
+                let value = 0;
+                switch(selection) {
+                    case "one":
+                        value = 1;
+                        break;
+                    case "two":
+                        value = 2;
+                        break;
+                    case "three":
+                        value = 3;
+                        break;
+                    case "four":
+                        value = 4;
+                        break;
+                    case "five":
+                        value = 5;
+                        break;
+                    case "six":
+                        value = 6;
+                        break;
                 }
+                for (let i = 0; i < 5; i++) {
+                    if (diceRoll.dice[i] === value) {
+                        this[selection].value += value;
+                        this.topScoreTotal += value;
+                    }
+                }
+                if (this.topScoreTotal >= 63) {
+                    this.bonus = 35;
+                    this.topScoreTotal += 35;
+                    this.totalScore += 35;
+                    $('#bonus').empty();
+                    $('#bonus').append(`<h3>${this.bonus}</h3>`);
+                }
+                this.totalScore = this.topScoreTotal + this.bottomScoreTotal;
+                this.writeValues("top", selection)
+                this[selection].scored = true;
+            } else {
+                alert(`You have already scored your ${selection.toUpperCase()}. Please pick something else.`);
             }
-            this.totalScore = this.topScoreTotal + this.bottomScoreTotal;
-            this.writeValues("top", selection)
         },
         addBottomScore: function (selection) {
-            const diceToString = diceRoll.sortedDice().join('');
-            let testCondition = "";
-            switch (selection) {
-                case "threeKind":
-                    testCondition = /111|222|333|444|555|666/;
-                    if(testCondition.test(diceToString)) {
+            if (this[selection].scored === false) {
+                const diceToString = diceRoll.sortedDice().join('');
+                let testCondition = "";
+                let testConditionTwo = "";
+                switch (selection) {
+                    case "threeKind":
+                        testCondition = /111|222|333|444|555|666/;
+                        if(testCondition.test(diceToString)) {
+                            this.sumDice(selection);
+                        }
+                        break;
+                    case "fourKind":
+                        testCondition = /1111|2222|3333|4444|5555|6666/;
+                        if(testCondition.test(diceToString)) {
+                            this.sumDice(selection);
+                        }
+                        break;  
+                    case "fullHouse":
+                        testCondition = /(111|222|333|444|555|666)(11|22|33|44|55|66)/;
+                        testConditionTwo = /(11|22|33|44|55|66)(111|222|333|444|555|666)/;
+                        if(testCondition.test(diceToString) || testConditionTwo.test(diceToString)) {
+                            this[selection].value += 25;
+                            this.bottomScoreTotal += 25;
+                        }
+                        break;
+                    case "small":
+                        const uniqueDice = _.uniq(diceToString, true).join('');
+                        testCondition = /1234|2345|3456/;
+                        if(testCondition.test(uniqueDice)) {
+                            this[selection].value += 30;
+                            this.bottomScoreTotal += 30;
+                        }
+                        break;
+                    case "large":
+                        testCondition = /12345|23456/;
+                        if(testCondition.test(diceToString)) {
+                            this[selection].value += 40;
+                            this.bottomScoreTotal += 40;
+                        }
+                        break;
+                    case "yahtzee":
+                        testCondition = /11111|22222|33333|44444|55555|66666/;
+                        if(testCondition.test(diceToString)) {
+                            console.log("HOLY CRAP, YOU GOT A YAHTZEE!!!!!!");
+                            this[selection].value += 50;
+                            this.bottomScoreTotal += 50;
+                        }
+                        break;
+                    case "chance":
                         this.sumDice(selection);
-                    }
-                    break;
-                case "fourKind":
-                    testCondition = /1111|2222|3333|4444|5555|6666/;
-                    if(testCondition.test(diceToString)) {
-                        this.sumDice(selection);
-                    }
-                    break;  
-                case "small":
-                    const uniqueDice = _.uniq(diceToString, true).join('');;
-                    testCondition = /1234|2345|3456/;
-                    if(testCondition.test(uniqueDice)) {
-                        this[selection] += 30;
-                        this.bottomScoreTotal += 30;
-                    }
-                    break;
-                case "large":
-                    testCondition = /12345|23456/;
-                    if(testCondition.test(diceToString)) {
-                        this[selection] += 40;
-                        this.bottomScoreTotal += 40;
-                    }
-                    break;
-                case "yahtzee":
-                    testCondition = /11111|22222|33333|44444|55555|66666/;
-                    if(testCondition.test(diceToString)) {
-                        console.log("HOLY CRAP, YOU GOT A YAHTZEE!!!!!!");
-                        this[selection] += 50;
-                        this.bottomScoreTotal += 50;
-                    }
-                    break;
-                case "chance":
-                    this.sumDice(selection);
-                    break;
+                        break;
+                }
+                this.totalScore = this.topScoreTotal + this.bottomScoreTotal;
+                this.writeValues("bottom", selection);
+                this[selection].scored = true;
+            } else {
+                alert(`You have already scored your ${selection.toUpperCase()}. Please pick something else.`);
             }
-            this.totalScore = this.topScoreTotal + this.bottomScoreTotal;
-            this.writeValues("bottom", selection);
         }
     }
 
@@ -150,8 +215,9 @@ $(document).ready(function() {
 });
 
 //  THINGS TO STILL IMPLEMENT
-//  full house regex check
+//  full house regex check -- DONE
 //  3 rolls max per turn
-//  marking scores already done to not be changed
+//  marking scores already done to not be changed -- DONE
 //  bonus top score
 //  marking dice to not be re-rolled
+//  make a separate re roll function 
